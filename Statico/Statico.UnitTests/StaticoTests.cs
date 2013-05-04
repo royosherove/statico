ced59695532433f197448c27424d2b401fb87bf5
unknown <roy@osherove.com>
2013-05-04 21:36:31 +0200

    initial simplistic test passing

--- Statico/Statico.UnitTests/StaticoTests.cs
+++ Statico/Statico.UnitTests/StaticoTests.cs
@@ -1,22 +1,52 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using NUnit.Framework;
+using NativeAssemblerInjection;
 
 namespace Statico.UnitTests
 {
+
+
+
     [TestFixture]
     public class StaticoTests
     {
         [Test]
         public void Fake_StaticWithInt_ReturnsFakeInt()
         {
-            AllCalls.To(() => ClassWithStaticMethods.StaticMethodThatReturns1()).WillReturn(2);
+            AllCalls.To(() => ClassWithStaticMethods.StaticMethodThatReturns1("a")).WillReturn(2);
 
+            Assert.AreEqual(2, ClassWithStaticMethods.StaticMethodThatReturns1("a"));
+        }
+    }
 
+    public class AllCalls
+    {
+        public static ReturnSpec To(Expression<Func<int>> thefunc )
+        {
+            MethodInfo toFake = typeof (ClassWithStaticMethods).GetMethod("StaticMethodThatReturns1", BindingFlags.Public | BindingFlags.Static);
+            MethodInfo replaement = new Func<int>(() => 2).Method;
+            MethodUtil.ReplaceMethod(replaement,toFake);
+            return new ReturnSpec();
+        }
+    }
+
+    public class ReturnSpec
+    {
+        public void WillReturn(int i)
+        {
+            
         }
     }
 
     public class ClassWithStaticMethods
     {
-        public int StaticMethodThatReturns1()
+        public static int StaticMethodThatReturns2()
+        {
+            return 2;
+        }
+        public static int StaticMethodThatReturns1(string s)
         {
             return 1;
         }
