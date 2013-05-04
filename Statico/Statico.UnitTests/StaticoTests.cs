9e5c8e787f2228dc244790aaaa1e967f95f87de8
unknown <roy@osherove.com>
2013-05-04 14:28:33 +0200

    the start begins..

---
+++ Statico/Statico.UnitTests/StaticoTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Statico.UnitTests
+{
+    [TestFixture]
+    public class StaticoTests
+    {
+        [Test]
+        public void Fake_StaticWithInt_ReturnsFakeInt()
+        {
+            AllCalls.To(() => ClassWithStaticMethods.StaticMethodThatReturns1()).WillReturn(2);
+
+
+        }
+    }
+
+    public class ClassWithStaticMethods
+    {
+        public int StaticMethodThatReturns1()
+        {
+            return 1;
+        }
+    }
+}
