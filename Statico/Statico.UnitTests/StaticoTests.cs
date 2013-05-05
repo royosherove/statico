af5ab15933c791f4bafdab0b7e77f72959f1c2ef
unknown <roy@osherove.com>
2013-05-05 23:01:14 +0200

    added futures and simple api

--- Statico/Statico.UnitTests/StaticoTests.cs
+++ Statico/Statico.UnitTests/StaticoTests.cs
@@ -8,16 +8,35 @@ namespace Statico.UnitTests
 {
 
 
+    class Person
+    {
+        public int Return1()
+        {
+            return 1;
+        } 
+    }
 
     [TestFixture]
     public class StaticoTests
     {
+        private Person p = new Person();
+        private Person p2 = new Person();
+
         [Test]
         public void Fake_StaticWithInt_ReturnsFakeInt()
         {
-            AllCalls.To(() => ClassWithStaticMethods.StaticMethodThatReturns1("a")).WillReturn(2);
+            AllCalls.To(() => ClassWithStaticMethods.StaticMethodThatReturns1()).Will(()=> 2);
 
-            Assert.AreEqual(2, ClassWithStaticMethods.StaticMethodThatReturns1("a"));
+            Assert.AreEqual(2, ClassWithStaticMethods.StaticMethodThatReturns1());
+        }
+        [Test]
+        public void Fake_NonStatic_WorksOnFutures()
+        {
+            AllCalls.To(() => new Person().Return1()).Will(() => 2);
+
+            Assert.AreEqual(2, new Person().Return1());
+            Assert.AreEqual(2, p.Return1());
+            Assert.AreEqual(2, p2.Return1());
         }
     }
 
@@ -25,18 +44,36 @@ namespace Statico.UnitTests
     {
         public static ReturnSpec To(Expression<Func<int>> thefunc )
         {
-            MethodInfo toFake = typeof (ClassWithStaticMethods).GetMethod("StaticMethodThatReturns1", BindingFlags.Public | BindingFlags.Static);
-            MethodInfo replaement = new Func<int>(() => 2).Method;
-            MethodUtil.ReplaceMethod(replaement,toFake);
-            return new ReturnSpec();
+            var call = thefunc.Body as MethodCallExpression;
+            MethodBase theMethod = call.Method;
+            ReturnSpec rs = new ReturnSpec(theMethod);
+            return rs;
         }
     }
 
     public class ReturnSpec
     {
-        public void WillReturn(int i)
+        public readonly MethodBase _toReplace;
+        public MethodInfo replaement;
+
+        public void Dispose()
         {
-            
+        }
+        public ReturnSpec(MethodBase toReplace)
+        {
+            _toReplace = toReplace;
+        }
+
+        public void Will<T>(Func<T> action)
+        {
+            replaement = action.Method;
+            MethodUtil.ReplaceMethod(replaement,_toReplace);
+        }
+
+        public void Will<T>(Action<T> action)
+        {
+            replaement = action.Method;
+            MethodUtil.ReplaceMethod(replaement,_toReplace);
         }
     }
 
@@ -46,7 +83,7 @@ namespace Statico.UnitTests
         {
             return 2;
         }
-        public static int StaticMethodThatReturns1(string s)
+        public static int StaticMethodThatReturns1()
         {
             return 1;
         }
